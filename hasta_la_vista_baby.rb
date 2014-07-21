729a12a91461ff3dfdc55096fac049b9776df529
Jonathan Lassoff <jonathan.lassoff@lookout.com>
2014-07-21 16:49:23 -0700

    Add initial POC Gist.

---
+++ hasta_la_vista_baby.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'fog'
+
+AWS_TOKENS = [
+  ['access key id #1', 'secret key id #1'],
+  ['access key id #2', 'secret key id #2']
+]
+
+AWS_TOKENS.each do |access_key_id, secret_access_key|
+  ec2_for_regions = Fog::Compute.new(:provider => 'AWS', :aws_access_key_id => access_key_id, :aws_secret_access_key => secret_access_key)
+  regions = ec2_for_regions.describe_regions.body['regionInfo'].map{|region|region['regionName']}
+  regions.each do |region|
+    ec2 = Fog::Compute.new(:provider => 'AWS', :aws_access_key_id => access_key_id, :aws_secret_access_key => secret_access_key, :region => region)
+    instances = ec2.servers
+    instances.each do |i|
+      unless (i.tags['Team'] and
+              i.tags['Team'].length > 0 and
+              i.tags['Service'] and
+              i.tags['Service'].length > 0)
+        i.destroy
+      end
+    end
+  end
+end
