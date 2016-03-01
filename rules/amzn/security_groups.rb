96fad9e2b83e08c4a89458281cfc8428e2768716
Ryan Greget <rgreget@gmail.com>
2016-03-01 11:35:24 -0800

    better default rules

--- rules/amzn/security_groups.rb
+++ rules/amzn/security_groups.rb
@@ -1,7 +1,24 @@
 module AMZN
   module SecurityGroupRules
+#  <Fog::Compute::AWS::SecurityGroup
+#    name="default",
+#    description="default security group",
+#    group_id="sg-12345678",
+#    ip_permissions=[{"groups"=>[], "ipRanges"=>[{"cidrIp"=>"0.0.0.0/0"}], "ipProtocol"=>"tcp", "fromPort"=>22, "toPort"=>22}],
+#    ip_permissions_egress=[],
+#    owner_id="123456789012",
+#    vpc_id=nil,
+#    tags={}
+#  >
     def evaluate(group)
-      expect(group.name).to eq('default')
+      group.ip_permissions.each do |ip_permission|
+        ip_permission['ipRanges'].each do |range|
+          if range.has_value? '0.0.0.0/0'
+            expect(ip_permission['fromPort']).to eq(80).or eq(443)
+          end
+        end
+      end
+      expect(group.vpc_id).to_not be_nil, 'expected group to belong to VPC'
     end
   end
 end
